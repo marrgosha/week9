d6dc001cd7b8def80c1a84bbef651fddcca6ccc5
marrgosha <marrgosha4@gmail.com>
2025-01-04 16:51:45 +0200

    2,3,4 task

---
+++ src/main/java/fi/margokomarova/week9/_main/_Main.java
@@ -0,0 +1,51 @@
+package fi.margokomarova.week9._main;
+
+import fi.margokomarova.week9.entity.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class _Main {
+    public static void main(String[] args){
+        List<Animal>animals=new ArrayList<>();
+        animals.add(new Animal("cat",2));
+        animals.add(new Animal("dog",3));
+        animals.add(new Animal("chicken",1));
+        animals.add(new Animal("crocodile",5));
+        animals.add(new Animal("fox",4));
+        animals.add(new Animal("elephant",6));
+        animals.add(new Animal("hare", 4));
+        animals.add(new Animal("lion", 7));
+        animals.add(new Animal("bird", 2));
+        animals.add(new Animal("mouse",1));
+
+        Stream<Animal> animalStream=animals.stream();
+        //task3
+        //long count=animalStream.filter(animal -> animal.getAge()>4)
+           //     .count();
+        //System.out.println(count);
+
+        //task5
+        //String names=animalStream.map(animal -> animal.getName().toString());
+
+
+       // Optional<String> names=animalStream.reduce((animal, animal2) -> animal.getName()+animal2.getName());
+        //System.out.println(names);
+
+        //task4
+        //Map<String,Animal>map=animalStream.collect(Collectors.toMap(animal -> animal.getName(),animal -> animal));
+        //System.out.println(map);
+
+        //task2
+        animalStream.sorted((animal1, animal2)->animal1.getAge()-animal2.getAge())
+                .forEach(animal -> System.out.println(animal.getName()+","+
+                        animal.getAge()));
+
+
+
+    }
+}
---
+++ src/main/java/fi/margokomarova/week9/entity/Animal.java
@@ -0,0 +1,23 @@
+package fi.margokomarova.week9.entity;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal (String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+
+
+
+}
